1cf313411558348e0987d4157bc15b0cab7bbcf8
vishal <vishal210893@gmail.com>
2020-04-19 03:44:44 +0530

    First Commit

---
+++ src/main/java/com/solum/kubernetes/poc/Controller.java
@@ -0,0 +1,44 @@
+package com.solum.kubernetes.poc;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class Controller {
+
+	@Autowired
+	private InstanceInformationService service;
+
+	@Autowired
+	private Environment env;
+
+	@GetMapping(value = "", produces = "application/json")
+	public ResponseEntity<HashMap<String, String>> welcomeMapping() {
+		SimpleDateFormat sdfDate = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		Date now = new Date();
+		String strDate = sdfDate.format(now);
+		HashMap<String, String> start = new HashMap<>();
+		start.put("msg", "Kubernetes service is running");
+		start.put("version", "1.0.0");
+		start.put("timestamp", strDate);
+		start.put("env", env.getProperty("environment"));
+		start.put("service", service.retrieveInstanceInfo());
+		return ResponseEntity.ok().body(start);
+	}
+
+	/// hello-world/path-variable/in28minutes
+	@GetMapping(path = "/hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello World, %s", name));
+	}
+}
---
+++ src/main/java/com/solum/kubernetes/poc/KubernetesPocApplication.java
@@ -0,0 +1,13 @@
+package com.solum.kubernetes.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubernetesPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KubernetesPocApplication.class, args);
+	}
+
+}
